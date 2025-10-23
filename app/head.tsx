e00b57dbe54f3e72d7faabd63dc2ab70eb15af0f
채종욱 <chaejongwook@192.168.nate.com>
2025-10-23 21:25:23 +0900

    최최종

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+    return (
+      <>
+        <meta
+          name="fc:miniapp"
+          content='{"version":"1","imageUrl":"https://fontify-nine.vercel.app/og.png","button":{"title":"Try Fontify ✨","action":{"type":"launch_miniapp","url":"https://fontify-nine.vercel.app"}}}'
+        />
+      </>
+    );
+  }
+  
